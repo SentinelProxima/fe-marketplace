1c2b218779e208ca11416b24d4686689d590d2f2
Akane <zarathustra47@yahoo.com>
2022-09-26 06:57:12 +0100

    removed some junk code

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,14 +1,4 @@
-import { useEffect, useState } from "react";
-
 const Basket = (props) => {
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-        fetch('https://proxima-market-api.herokuapp.com/api/items')
-        .then(res => res.json())
-        .then((data) => {
-            setItems(data.items);
-        })
-    }, []);
     return (
         <section>
         {props.basket.filter((item) => item).map((item) => {
